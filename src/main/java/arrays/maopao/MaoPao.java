cd26a07e4e60821298a01a2aa7ed3386c1a13081
Dragon <Dragon@qq.com>
2022-05-13 17:02:43 +0800

    冒泡排序：冒泡排序注释修改

--- src/main/java/arrays/maopao/MaoPao.java
+++ src/main/java/arrays/maopao/MaoPao.java
@@ -10,16 +10,16 @@ public class MaoPao {
     public static void main(String[] args) {
         int[] arr = {7, 2, 1, 3, 6, 5, 4};
         //冒泡排序
-//        for (int i=arr.length-1;i>0;i--){
-//            for (int j = 0; j < i; j++) {
-//                if (arr[j] > arr[j+1]){
-//                    int temp;
-//                    temp=arr[j];
-//                    arr[j]=arr[j+1];
-//                    arr[j+1]=temp;
-//                }
-//            }
-//        }
+        for (int i=arr.length-1;i>0;i--){
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j+1]){
+                    int temp;
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
         //选择排序
         for (int i = 0; i < arr.length - 1; i++) {
             int min = i;//假设第一个元素就是最小的
